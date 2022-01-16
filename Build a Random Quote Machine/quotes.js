ccc656569c0b145e31c7dd3ce09912b41987ee5d
Mário Almeida <khrome666@gmail.com>
2022-01-16 19:38:13 +0000

    Random quotes almost

--- Build a Random Quote Machine/quotes.js
+++ Build a Random Quote Machine/quotes.js
@@ -91,47 +91,45 @@ class TweetQuote extends React.Component
 
 class QuoteBox extends React.Component
 {
-    quotes =[
-        {
-            text: "The purpose of our lives is to be happy.",
-            author: "Dalai Lama"
-        },
-        {
-            text: "Life is what happens when you're busy making other plans.",
-            author: "John Lennon"
-        },
-        {
-            text: "Get busy living or get busy dying.",
-            author:  "Stephen King"
-        },
-        {
-            text: "You only live once, but if you do it right, once is enough.",
-            author: "Mae West"
-        }
-    ];
-
     constructor(props)
     {
         super(props);
-        let randomQuote = Math.floor(Math.random() * 4);
         this.state = {
-            text: quotes[randomQuote].text,
-            author: quotes[randomQuote].author
+            quotes: [
+                {
+                    text: "The purpose of our lives is to be happy.",
+                    author: "Dalai Lama"
+                },
+                {
+                    text: "Life is what happens when you're busy making other plans.",
+                    author: "John Lennon"
+                },
+                {
+                    text: "Get busy living or get busy dying.",
+                    author:  "Stephen King"
+                },
+                {
+                    text: "You only live once, but if you do it right, once is enough.",
+                    author: "Mae West"
+                }
+            ]
         };
     }
 
     render()
     {
+        let randomQuote = Math.floor(Math.random() * 4);
+
         return React.createElement(
             "div",
             {
                 id: "quote-box"
             },
         React.createElement(Quote, {
-                text: this.state.text
+                text: this.state.quotes[randomQuote].text
             }),
         React.createElement(Author, {
-                author: this.state.author
+                author: this.state.quotes[randomQuote].author
             }),
         React.createElement(NewQuote, null),
         React.createElement(TweetQuote, null)
