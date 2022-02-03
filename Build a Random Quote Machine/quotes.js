9b4365e2bfeb84c3e37584bf61b354f09bb264a2
Mário Almeida <khrome666@gmail.com>
2022-02-03 20:51:29 +0000

    Final JS

--- Build a Random Quote Machine/quotes.js
+++ Build a Random Quote Machine/quotes.js
@@ -1,18 +1,23 @@
-'use strict';
+"use strict";
 
-const quotes = [{
-    text: "The purpose of our lives is to be happy.",
-    author: "Dalai Lama"
-}, {
-    text: "Life is what happens when you're busy making other plans.",
-    author: "John Lennon"
-}, {
-    text: "Get busy living or get busy dying.",
-    author: "Stephen King"
-}, {
-    text: "You only live once, but if you do it right, once is enough.",
-    author: "Mae West"
-}];
+const quotes = [
+    {
+        text: "The purpose of our lives is to be happy.",
+        author: "Dalai Lama"
+    }, 
+    {
+        text: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon"
+    }, 
+    {
+        text: "Get busy living or get busy dying.",
+        author: "Stephen King"
+    }, 
+    {
+        text: "You only live once, but if you do it right, once is enough.",
+        author: "Mae West"
+    }
+];
 
 const Quote = props =>
 {
@@ -47,23 +52,18 @@ const TweetQuote = () =>
 
 const QuoteBox = () =>
 {
-    const randomQuote = () => quotes[Math.floor(Math.random() * 4)];
-
-    const callbackQuote = () => {
-        setQuote(randomQuote());
-    }
-
+    const randomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
+    const callbackQuote = () => setQuote(randomQuote());
     const [activeQuote, setQuote] = React.useState(randomQuote());
 
-    return React.createElement("div", {
-        id: "quote-box"
-    }, React.createElement(Quote, {
-        quote: activeQuote
-    }), React.createElement(Author, {
-        quote: activeQuote
-    }), React.createElement(NewQuote, {
-        callback: callbackQuote
-    }), React.createElement(TweetQuote, null));
+    return (
+        React.createElement("div", {id: "quote-box"}, 
+            React.createElement(Quote, {quote: activeQuote}), 
+            React.createElement(Author, {quote: activeQuote}), 
+            React.createElement(NewQuote, {callback: callbackQuote}), 
+            React.createElement(TweetQuote, null)
+        )
+    );
 };
 
 const domContainer = document.querySelector('#main-box');
