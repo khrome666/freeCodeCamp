3a78d3262383ff8774b8728ac10d472d5f5a1ccb
khrome666 <khrome666@gmail.com>
2022-02-10 17:36:23 +0000

    Seven

--- Javascript Calculator/calculator.js
+++ Javascript Calculator/calculator.js
@@ -3,40 +3,116 @@
 // Definição
 function Key(props)
 {
-    return React.createElement(
-        "button",
-        {
-            className: `btn btn-${props.color} w-100 h-100`
-        },
-        props.op
-    );
+    return React.createElement("button", {
+        id: `${props.id}`,
+        className: `btn btn-${props.color} w-100 h-100`,
+        onClick: () => console.log(props.op)
+    }, props.op);
 }
 
-function Keyboard(props)
+
+function Display(props)
 {
-    return React.createElement(
-        "tr",
-        null,
-        React.createElement(
-            "td",
-            null,
-            React.createElement(Key, {
-                color: "secondary",
-                op: "0"
-            })
-        ),
-        React.createElement(
-            "td",
-            null,
-            React.createElement(Key, {
-                color: "secondary",
-                op: "."
-            })
-        )
-    );
+    return React.createElement("p", {
+        id: `${props.id}`,
+        className: "bg-dark text-warning text-end display-4 p-2"
+    }, props.value);
 }
 
+
+function Calculator(props)
+{
+    const storeDisplayValue = op => setDisplayValue(op);
+
+    const [displayValue, setDisplayValue] = React.useState("0");
+    return /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+        colSpan: "4"
+    }, /*#__PURE__*/React.createElement(Display, {
+        id: "display",
+        value: displayValue
+    }))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+        colSpan: "2"
+    }, /*#__PURE__*/React.createElement(Key, {
+        id: "clear",
+        color: "danger",
+        op: "AC"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "divide",
+        color: "primary",
+        op: "/"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "multiply",
+        color: "primary",
+        op: "*"
+    }))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "seven",
+        color: "secondary",
+        op: "7",
+        onClick: () => storeDisplayValue("7")
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "eight",
+        color: "secondary",
+        op: "8"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "nine",
+        color: "secondary",
+        op: "9"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "subtract",
+        color: "primary",
+        op: "-"
+    }))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "four",
+        color: "secondary",
+        op: "4"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "five",
+        color: "secondary",
+        op: "5"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "six",
+        color: "secondary",
+        op: "6"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "add",
+        color: "primary",
+        op: "+"
+    }))), /*#__PURE__*/React.createElement("tr", {
+        className: "h-100"
+    }, /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "one",
+        color: "secondary",
+        op: "1"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "two",
+        color: "secondary",
+        op: "2"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "three",
+        color: "secondary",
+        op: "3"
+    })), /*#__PURE__*/React.createElement("td", {
+        rowSpan: "2",
+        className: "h-100"
+    }, /*#__PURE__*/React.createElement(Key, {
+        id: "equals",
+        color: "success",
+        op: "="
+    }))), /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+        colSpan: "2"
+    }, /*#__PURE__*/React.createElement(Key, {
+        id: "zero",
+        color: "secondary",
+        op: "0"
+    })), /*#__PURE__*/React.createElement("td", null, /*#__PURE__*/React.createElement(Key, {
+        id: "decimal",
+        color: "secondary",
+        op: "."
+    }))));
+}
+
+;
 
 // Visualização
-let domContainer = document.querySelector('#pointkey');
-ReactDOM.render(React.createElement(Keyboard), domContainer);
+let domContainer = document.querySelector('#calculator');
+ReactDOM.render(React.createElement(Calculator), domContainer);
--- Javascript Calculator/calculator_jsx.js
+++ Javascript Calculator/calculator_jsx.js
@@ -1,19 +1,59 @@
-function Key(props) {
+import React, { useState } from "react";
+
+function Key(props)
+{
     return (
-      	<button className={`btn btn-${props.color} w-100 h-100`}>{props.op}</button>
+        <button id={`${props.id}`} className={`btn btn-${props.color} w-100 h-100`} onClick={() => console.log(props.op)}>{props.op}</button>
     );
 }
 
-function Keyboard(props) {
-  return (
-    <tr>
-      <td>
-        <Key color="secondary" op="0" />
-      </td>
-      <td>
-        <Key color="secondary" op="." />
-      </td>
-    </tr>
-  );
+function Display(props)
+{
+    return (
+        <p id={`${props.id}`} className="bg-dark text-warning text-end display-4 p-2">{props.Value}</p>
+    );
+}
+
+function Calculator(props)
+{
+    const storeDisplayValue = (op) => setDisplayValue(op);
+    const [displayValue, setDisplayValue] = React.useState("0");
+
+    return (
+        <tbody>
+            <tr>
+                <td colSpan="4">
+                    <Display id="display" value={displayValue} />
+                </td>
+            </tr>
+            <tr>
+                <td colSpan="2"><Key id="clear" color="danger" op="AC" /></td>
+                <td><Key id="divide" color="primary" op="/" /></td>
+                <td><Key id="multiply" color="primary" op="*" /></td>
+            </tr>
+            <tr>
+                <td><Key id="seven" color="secondary" op="7" onClick={() => storeDisplayValue("7")} /></td>
+                <td><Key id="eight" color="secondary" op="8" /></td>
+                <td><Key id="nine" color="secondary" op="9" /></td>
+                <td><Key id="subtract" color="primary" op="-" /></td>
+            </tr>
+            <tr>
+                <td><Key id="four" color="secondary" op="4" /></td>
+                <td><Key id="five" color="secondary" op="5" /></td>
+                <td><Key id="six" color="secondary" op="6" /></td>
+                <td><Key id="add" color="primary" op="+" /></td>
+            </tr>
+            <tr className="h-100">
+                <td><Key id="one" color="secondary" op="1" /></td>
+                <td><Key id="two" color="secondary" op="2" /></td>
+                <td><Key id="three" color="secondary" op="3" /></td>
+                <td rowSpan="2" className="h-100"><Key id="equals" color="success" op="=" /></td>
+            </tr>
+            <tr>
+                <td colSpan="2"><Key id="zero" color="secondary" op="0" /></td>
+                <td><Key id="decimal" color="secondary" op="." /></td>
+            </tr>
+        </tbody>
+    );
 };
 
