2762eb309032654ec4cad7916562c9b38b648dde
Mário Almeida <khrome666@gmail.com>
2022-02-14 21:52:21 +0000

    CSS kicks in

--- scrimba/P01/index.js
+++ scrimba/P01/index.js
@@ -1,14 +1,32 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 
-function MyReasons() {
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img className="nav-logo" src="./react-logo.png" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>&copy; 2022 Almeida development. All rights reserved.</small>
+        </footer>
+    );
+}
+
+function MainContent() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's cool</li>
@@ -16,9 +34,16 @@ function MyReasons() {
                 <li>Did I say cool?</li>
                 <li>Money!!!</li>
             </ol>
-            <footer>
-                &copy; 2022 Almeida development. All rights reserved.
-            </footer>
+        </div>
+    );
+}
+
+function MyReasons() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
