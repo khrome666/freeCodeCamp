909c50b8ac9b5c4b61551e12dbb49cb117427799
Mário Almeida <khrome666@gmail.com>
2022-02-14 09:48:04 +0000

    Project 1

--- scrimba/P01/index.js
+++ scrimba/P01/index.js
@@ -1,4 +1,26 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 
+function MyReasons() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's cool</li>
+                <li>Easy to learn (not!)</li>
+                <li>Did I say cool?</li>
+                <li>Money!!!</li>
+            </ol>
+            <footer>
+                &copy; 2022 Almeida development. All rights reserved.
+            </footer>
+        </div>
+    );
+}
 
+ReactDOM.render(<MyReasons />, document.getElementById("root"));
