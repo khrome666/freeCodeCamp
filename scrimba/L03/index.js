9a19ef16ba0534e152c650dcf735d0eba7c2e2aa
khrome666 <khrome666@gmail.com>
2022-02-15 15:09:03 +0000

    Scrimba moved

--- scrimba/L03/index.js
+++
@@ -1,6 +0,0 @@
-// ReactDOM.render(<i1>Hello, React!</h1>, document.getElementById("root"));
-
-const h1 = document.createElement("h1");
-h1.textContent = "This is a journey into React";
-h1.className = "header";
-document.getElementById("root").appendChild(h1);
