877c2624cce3d1827c15671c4909a0ed746405ed
Mário Almeida <khrome666@gmail.com>
2022-02-15 07:32:01 +0000

    No CDN

--- scrimba/P01/index.js
+++ scrimba/P01/index.js
@@ -1,24 +1,10 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
-
-function Header() {
-    return (
-        <header>
-            <nav className="nav">
-                <img className="nav-logo" src="./react-logo.png" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+import Header from "./Header.js"
 
 function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <small>&copy; 2022 Almeida development. All rights reserved.</small>
         </footer>
     );
@@ -26,7 +12,7 @@ function Footer() {
 
 function MainContent() {
     return (
-        <div>
+        <div className="main-content">
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's cool</li>
