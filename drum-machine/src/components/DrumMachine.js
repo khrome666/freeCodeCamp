c4c33453c4ff2059490c6e7b1b169b1287eba3c7
khrome666 <khrome666@gmail.com>
2022-03-07 12:17:18 +0000

    Setup

---
+++ drum-machine/src/components/DrumMachine.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Display from './Display';
+import DrumPad from './DrumPad';
+
+export default function DrumMachine()
+{
+    const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+    drumPadArray = 
+    return (
+        <Display />
+
+    );
+}
